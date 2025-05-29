e18c0fcd9b77d7845080366155b885edb5597d59
sovanarith3 <Sosovanarith3@gmail.com>
2025-05-29 08:41:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-numpy
-schedule
-psycopg2-binary
-beautifulsoup4
-selenium
-flask
-gunicorn
+from flask import Flask, render_template
+import indra
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    status = indra.get_status()
+    return render_template('index.html', status=status)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
