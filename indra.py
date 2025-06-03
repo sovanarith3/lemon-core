0d8f6a37d0a017fafaa18f31830494a69cfd78b8
sovanarith3 <Sosovanarith3@gmail.com>
2025-06-03 13:35:49 -0400

    Updated indra.py to store explored data

--- indra.py
+++ indra.py
@@ -13,6 +13,8 @@ class IndraAI:
         self.counter_file = "counter.json"
         self.counter = self._load_counter()
         self.visited_urls = set()
+        self.knowledge_file = "knowledge.json"
+        self.knowledge = self._load_knowledge()
 
     def _load_counter(self):
         if os.path.exists(self.counter_file):
@@ -24,6 +26,16 @@ class IndraAI:
     def _save_counter(self):
         with open(self.counter_file, 'w') as f:
             json.dump({"visits": self.counter}, f)
+
+    def _load_knowledge(self):
+        if os.path.exists(self.knowledge_file):
+            with open(self.knowledge_file, 'r') as f:
+                return json.load(f)
+        return {}
+
+    def _save_knowledge(self):
+        with open(self.knowledge_file, 'w') as f:
+            json.dump(self.knowledge, f)
 
     def get_status(self):
         return "Running"
@@ -52,7 +64,13 @@ class IndraAI:
                 link = urljoin(url, a_tag['href'])
                 if link.startswith('http') and link not in self.visited_urls:
                     links.append(link)
-            return {"url": url, "text": text[:500], "links": links[:5]}  # Limit links to 5
+            result = {"url": url, "text": text[:500], "links": links[:5]}
+
+            # Store knowledge
+            self.knowledge[url] = result
+            self._save_knowledge()
+
+            return result
         except Exception as e:
             return {"url": url, "error": str(e)}
 
