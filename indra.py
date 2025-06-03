c93e6db5090a086c1ebc0ba0707dcf24740a55af
sovanarith3 <Sosovanarith3@gmail.com>
2025-06-03 12:46:14 -0400

    Updated indra.py to persist visit counter

--- indra.py
+++ indra.py
@@ -1,17 +1,32 @@
 import logging
+import json
+import os
 
 logging.basicConfig(level=logging.INFO)
 logging.info("IndraAI initialized")
 
 class IndraAI:
     def __init__(self):
-        self.counter = 0
+        self.counter_file = "counter.json"
+        self.counter = self._load_counter()
+
+    def _load_counter(self):
+        if os.path.exists(self.counter_file):
+            with open(self.counter_file, 'r') as f:
+                data = json.load(f)
+                return data.get("visits", 0)
+        return 0
+
+    def _save_counter(self):
+        with open(self.counter_file, 'w') as f:
+            json.dump({"visits": self.counter}, f)
 
     def get_status(self):
         return "Running"
 
     def get_memory(self):
         self.counter += 1
+        self._save_counter()
         return {"visits": self.counter}
 
 if __name__ == "__main__":
