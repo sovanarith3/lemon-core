bf640922aa79b78375e104938deb4f65cce01111
sovanarith3 <Sosovanarith3@gmail.com>
2025-06-03 12:08:35 -0400

    Updated indra.py to track visits

--- indra.py
+++ indra.py
@@ -4,10 +4,15 @@ logging.basicConfig(level=logging.INFO)
 logging.info("IndraAI initialized")
 
 class IndraAI:
+    def __init__(self):
+        self.counter = 0
+
     def get_status(self):
         return "Running"
+
     def get_memory(self):
-        return {}
+        self.counter += 1
+        return {"visits": self.counter}
 
 if __name__ == "__main__":
     indra = IndraAI()
