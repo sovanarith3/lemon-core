0dcefdb7d420cafed0e50b7b13ab8b05c9179179
sovanarith3 <Sosovanarith3@gmail.com>
2025-06-03 11:51:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 import logging
 import os
+import indra
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug("Starting app.py")
 
 app = Flask(__name__)
-app.config['STATIC_FOLDER'] = 'static'
 app.config['DEBUG'] = True
 logging.debug("Flask app initialized")
 
@@ -18,10 +18,14 @@ if os.path.exists(templates_dir):
 else:
     logging.error("Templates directory does not exist")
 
+indra_instance = indra.IndraAI()
+
 @app.route('/')
 def index():
+    status = indra_instance.get_status()
+    memory = indra_instance.get_memory()
     logging.debug("Rendering index page")
-    return render_template('index.html')
+    return render_template('index.html', status=status, memory=memory)
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8000))
